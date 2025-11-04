de6fa2a7bf86fd2d7d95b32ba0c922963bc0255b
細谷胡奈 <s1323132@cst.otsuma.ac.jp>
2025-11-04 04:06:07 +0000

    Complete game implementation and UI improvements

--- server.js
+++ server.js
@@ -8,40 +8,33 @@ const io = new Server(server);
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-  // Chat functionality (for index.html)
-  socket.on('user connected', (clientId) => {
-    socket.clientId = clientId;
-    console.log(clientId + ' connected');
-    socket.emit('welcome', clientId);
-    socket.broadcast.emit('user joined', clientId);
-  });
+    // クライアントが接続したときのログ
+    socket.on('user connected', (clientId) => {
+        socket.clientId = clientId;
+        console.log(clientId + ' connected');
+    });
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-  
-  // Room functionality (for sensor apps)
-  socket.on('join', (room) => {
-    socket.join(room);
-    console.log(`Client joined room: ${room}`);
-  });
-  
-  // Sensor data handling
-  socket.on('sensor', (data) => {
-    // センサーデータを'game'ルームにいる他のクライアントに送信する
-    socket.to('game').emit('sensor', data);
-  });
+    // クライアントが「game」ルームに入ったとき
+    socket.on('join', (room) => {
+        socket.join(room);
+        console.log(`Client joined room: ${room}`);
+    });
+    
+    // スマホからセンサーデータを受信し、全ゲームクライアントにリレー
+    socket.on('sensor', (data) => {
+        // raw data { id, a, b, g, z } をそのまま 'sensor' イベントとしてブロードキャスト
+        io.to('game').emit('sensor', data); 
+    });
 
-  socket.on('disconnect', () => {
-    if (socket.clientId) {
-      console.log(socket.clientId + ' disconnected');
-      io.emit('user left', socket.clientId);
-    } else {
-      console.log('Client disconnected');
-    }
-  });
+    socket.on('disconnect', () => {
+        if (socket.clientId) {
+            console.log(socket.clientId + ' disconnected');
+        } else {
+            console.log('Client disconnected');
+        }
+    });
 });
 
 server.listen(8080, () => {
-  console.log('listening on *:8080');
+    console.log('listening on *:8080');
 });
