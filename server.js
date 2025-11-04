2a72bdbb879909fe123f0b1ae81d5c7ab6b7b0f3
細谷胡奈 <s1323132@cst.otsuma.ac.jp>
2025-11-04 05:43:59 +0000

    Feature: Complete code formatting and fix game replay logic

--- server.js
+++ server.js
@@ -8,36 +8,31 @@ const io = new Server(server);
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-    // クライアントが接続したときのログ
-    socket.on('user connected', (clientId) => {
-        socket.clientId = clientId;
-        console.log(clientId + ' connected');
-    });
+	socket.on('user connected', (clientId) => {
+		socket.clientId = clientId;
+		console.log(clientId + ' connected');
+	});
 
-    // クライアントが「game」ルームに入ったとき
-    socket.on('join', (room) => {
-        socket.join(room);
-        console.log(`Client joined room: ${room}`);
-    });
-    
-    // スマホからセンサーデータを受信し、全ゲームクライアントにリレー
-    socket.on('sensor', (data) => {
-        // raw data { id, a, b, g, z } をそのまま 'sensor' イベントとしてブロードキャスト
-        io.to('game').emit('sensor', data); 
-    });
+	socket.on('join', (room) => {
+		socket.join(room);
+		console.log(`Client joined room: ${room}`);
+	});
+	
+	socket.on('sensor', (data) => {
+		io.to('game').emit('sensor', data); 
+	});
 
-    socket.on('disconnect', () => {
-        if (socket.clientId) {
-            console.log(socket.clientId + ' disconnected');
-        } else {
-            console.log('Client disconnected');
-        }
-    });
+	socket.on('disconnect', () => {
+		if (socket.clientId) {
+			console.log(socket.clientId + ' disconnected');
+		} else {
+			console.log('Client disconnected');
+		}
+	});
 });
 
-// ★修正点：ポートを環境変数から取得し、なければ8080を使用する★
 const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
-    console.log(`listening on *:${PORT}`);
+	console.log(`listening on *:${PORT}`);
 });
